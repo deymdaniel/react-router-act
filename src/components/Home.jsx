a7d012f87a3e546149b7e547e9f842eea73a96e6
deymdaniel <danielbiacaniii@gmail.com>
2025-06-14 22:05:48 +0800

    centered home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className='home-container flex flex-col items-center  min-h-screen bg-gray-100'>
+    <div className='home-container flex flex-col items-center justify-center min-h-screen bg-gray-100'>
       <h2 className='text-2xl font-bold mb-4'>Home Page</h2>
       <nav className='mb-4'>
         <Link to='/about' className='text-blue-500 hover:underline'>
